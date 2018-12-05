67e8ef031a537db3dc88015efe2794e74bbad416
chrome303 <chrome303@gmail.com>
2018-12-05 14:39:04 +0100

    correst linting errors

--- test_calculator.py
+++ test_calculator.py
@@ -16,6 +16,5 @@ class TestCalculator:
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
 
-
     def test_division(self):
-        assert 2 == calculator.divide(4,2)
+        assert 2 == calculator.divide(4, 2)
